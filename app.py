f04a92b46883a0e0daf032c4e242872fde9c0e02
Enzo <enzofabian3007@gmail.com>
2025-06-11 12:39:46 -0500

    Mensaje

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +9,8 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
